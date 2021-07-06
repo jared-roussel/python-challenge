c00af28fa7ac04cb4d6468f40e32873f8a653ed3
jared.roussel@outlook.com <jared.roussel@outlook.com>
2021-07-06 00:39:35 -0400

    Set initial list values

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,15 +4,26 @@ import csv
 
 #Identify location of csv file
 file_name = os.path.join('PyBank','Resources','budget_data.csv')
-fields = []
-rows = []
+
+#Initialize empty lists for tracking changes
+header_names = []
+monthly_dates = []
+profit_losses = []
+
 
 #Open and read csv file
 with open(file_name,"r") as csv_file:
     csv_reader = csv.reader(csv_file,delimiter=",")
     csv_header = next(csv_reader)
-    fields = csv_header
-    print(fields)
+    header_names = csv_header
+    print(header_names)
     
+    #for rows in csv_reader:
+        #print(rows)
 
+    for rows in csv_reader:
+        monthly_dates.append(rows[0])
+        profit_losses.append(rows[1])
+        # print(monthly_dates)
+        #print(profit_losses)
 
